45bdaff328c9ff719a524caadb823432a8770759
AlexanderDergachev <31667079+AlexanderDergachev@users.noreply.github.com>
2020-07-07 03:08:23 +0300

    Updates

---
+++ precache-manifest.7dcc6bc779ba471f1beccaada8cc1ccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4dab6ec1696220a9dc29ae6677502d3",
+    "url": "/dayry-app/index.html"
+  },
+  {
+    "revision": "c74cd44946a21564d04b",
+    "url": "/dayry-app/static/css/main.171e371b.chunk.css"
+  },
+  {
+    "revision": "dc995cdc56896ef52ef0",
+    "url": "/dayry-app/static/js/2.8ee5897e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dayry-app/static/js/2.8ee5897e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c74cd44946a21564d04b",
+    "url": "/dayry-app/static/js/main.e4c42254.chunk.js"
+  },
+  {
+    "revision": "07e39d364750b085e1d9",
+    "url": "/dayry-app/static/js/runtime-main.3fe81f8c.js"
+  }
+]);
